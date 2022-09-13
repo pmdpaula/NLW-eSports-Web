310532fe9f945a3cb4cce1f5cd7a894dd0faafd8
Pedro de Paula (remoto) <pmdpaula@yahoo.com.br>
2022-09-13 19:03:06 -0300

    ignite - fim da aula 02

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      colors: {},
+      backgroundImage: {
+        galaxy: "url(/background-galaxy.png)",
+        'nlw-gradient': 'linear-gradient(90deg, #9572FC 20.08%, #437EAD 26.94%, #E1D55D 85.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%);',
+      }
+    },
+  },
+  plugins: [],
+}
